66b5f2961adb9eebad0dee0fbd47d39d90a18db9
ZStachura <zmstachura@gmail.com>
2022-08-17 12:14:14 +0200

    0.3 -> Sign in and Register with validation

--- Mini-CRM/src/app/authentication/authentication.component.ts
+++ Mini-CRM/src/app/authentication/authentication.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import Validation from './utils/validation';
 
 @Component({
   selector: 'app-authentication',
@@ -7,15 +9,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   signIn:boolean=false;
   signUp:boolean=true;
 
+  //Register
+  form: FormGroup = new FormGroup({
+    firstname: new FormControl(''),
+    lastname:new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
+  submitted = false;
+
+  //LogIn
+  logform:FormGroup=new FormGroup({
+    email:new FormControl(''),
+    password:new FormControl('')
+  });
+  log=false;
 
   ngOnInit(): void {
+    //Register
+    this.form = this.formBuilder.group(
+      {
+        firstname: ['',[ Validators.required,Validators.pattern('[a-zA-Z]*')]],
+        lastname:['',[ Validators.required,Validators.pattern('[a-zA-Z]*')]],
+        username: ['',Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required]
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
+    //LogIn
+    this.logform=this.formBuilder.group(
+      {
+        email:['',[Validators.required,Validators.email]],
+        password:['',[Validators.required]]
+      }
+    )
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  //registration
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
+
+  //logIn
+   tryToLogIn():void{
+    this.log=true;
+    if(this.logform.invalid)
+    {
+      return;
+    }
+    console.log(JSON.stringify(this.logform.value,null,2));
+   }
+
+   logreset():void{
+    this.log=false;
+    this.logform.reset();
+   }
+
+  //overlays
   changeOverlay(){
     this.signIn=!this.signIn;
     this.signUp=!this.signUp;
--- Mini-CRM/src/app/authentication/authentication.module.ts
+++ Mini-CRM/src/app/authentication/authentication.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
@@ -11,7 +12,8 @@ import { AuthenticationComponent } from './authentication.component';
   ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthenticationModule { }
